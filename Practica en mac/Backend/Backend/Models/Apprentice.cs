23033300551ba64ce4db6006ec99d314200c5a28
sebasegura97 <sebasegura97@gmail.com>
2017-10-10 08:59:45 -0300

    Cambios

---
+++ Practica en mac/Backend/Backend/Models/Apprentice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Backend.Models
+{
+    public class Apprentice
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Id {
+            set; get;           //Esta es la forma rapida de getters y setters
+        }
+
+        //public void SetId(int i)   -Getters y setters, para no acceder
+        //{                           directamente al id, sino mediante este metodo.
+        //    id = i;                 Esta es la forma larga de hacerlo en C#
+        //}
+        //public int GetId(){
+        //    return id;
+        //}
+    }
+}
